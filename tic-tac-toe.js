5953ed8a6e405f42bed24267519d92de391f743f
tywaine <peterstywaine@gmail.com>
2024-10-23 21:41:58 -0500

    Completed Exercise 4. Forgot to create the game-implementation branch so I just did that

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,23 +1,39 @@
 document.addEventListener('DOMContentLoaded', function() {
     const squares = document.querySelectorAll('#board div');
+    let status = document.getElementById('status');
+
     let player = 'X';
     const board = Array(9).fill(null);
+    let isGameActive = true
 
     squares.forEach((square, i) => {
         square.classList.add('square');
 
         square.addEventListener('click', function() {
-            if (!board[i]) {
+            if (!board[i] && isGameActive) {
                 board[i] = player;
                 square.textContent = player;
                 square.classList.add(player);
+
+                if(isWinnerAvailable(board, player)){
+                    status.textContent = "Congratulations! " + player + " is the Winner!";
+                    status.classList.add('you-won');
+                    isGameActive = false;
+                    return;
+                }
+
+                if(isDraw(board)){
+                    status.textContent = "Game was a Draw!";
+                    isGameActive = false;
+                    return;
+                }
 
                 player = (player === 'X') ? 'O' : 'X';
             }
         });
 
         square.addEventListener('mouseover', function (){
-            if(!board[i]){
+            if(!board[i] && isGameActive){
                 square.classList.add('hover');
                 square.classList.add(player);
             }
@@ -26,11 +42,38 @@ document.addEventListener('DOMContentLoaded', function() {
         square.addEventListener('mouseout', function() {
             square.classList.remove('hover');
 
-            if(!board){
+            if(!board[i]){
                 square.classList.remove(player);
             }
-
         })
-
     });
 });
+
+
+function isWinnerAvailable(board, player){
+    for(let i = 0; i < 9; i+=3){
+        if(board[i] === player && board[i + 1] === player && board[i + 2] === player){
+            return true;
+        }
+    }
+
+    for(let i = 0; i < 3; i++){
+        if(board[i] === player && board[i + 3] === player && board[i + 6] === player){
+            return true;
+        }
+    }
+
+    return (board[0] === player && board[4] === player && board[8] === player) ||
+        (board[2] === player && board[4] === player && board[6] === player);
+}
+
+
+function isDraw(board){
+    for(let i = 0; i < board.length; i++){
+        if(board[i] === null){
+            return false;
+        }
+    }
+
+    return true;
+}
